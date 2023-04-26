083dbd72dd475c1bbfe07559aabba6094fba2df5
tedi <hkanelhs@yahoo.gr>
2023-04-26 22:07:05 +0300

    Lints

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,11 +2,11 @@
 
 # Various lints
 
-set -euxo pipefail
+set -euo pipefail
 
 
 # Lind all bash scripts in the directory recursively.
 find . -type f -not -path '*/\.git/*'  -exec file {} + | \
           grep "shell script" | \
           cut -d: -f1 | \
-          xargs -I{} sh -c 'shellcheck --shell=bash --external-sources "{}"'
+          xargs -I{} sh -c 'shellcheck --shell=bash --check-sourced --external-sources "{}"'
